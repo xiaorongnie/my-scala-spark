cfd47f73381a2dd1ea5bf9fd0da18994752b601f
xiaorongnie <xiaorongnie@transcodegroup.com>
2021-10-25 16:35:04 +0800

    SparkCore_03_Action

--- src/main/scala/com/transcodegroup/HelloWord.scala
+++ src/main/scala/com/transcodegroup/HelloWord.scala
@@ -5,6 +5,35 @@ import lombok.Data
 @Data
 object HelloWord {
   def main(args: Array[String]): Unit = {
-    print("hello word")
+    val list = Array.range(1, 10)
+
+    // partition
+    println("partition...")
+    val partition = list.partition(_ < 5)
+    println(partition._1.mkString(","))
+    println(partition._2.mkString(","))
+
+    // groupBy
+    println("groupBy...")
+    val groupBy = list.groupBy(_ % 3)
+    println(groupBy.getClass)
+    for (element <- groupBy) {
+      println(element._2.mkString(","))
+    }
+    // grouped
+    println("grouped...")
+    val grouped = list.grouped(5)
+    println(grouped.getClass)
+    while (grouped.hasNext){
+      println(grouped.next().mkString(","))
+    }
+
+    // sliding
+    println("sliding...")
+    val sliding = list.sliding(5)
+    println(sliding.getClass)
+    while (sliding.hasNext){
+      println(sliding.next().mkString(","))
+    }
   }
 }
---
+++ src/main/scala/com/transcodegroup/SparkCore_03_Action.scala
@@ -0,0 +1,71 @@
+package com.transcodegroup
+
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.{SparkConf, SparkContext}
+import org.slf4j.LoggerFactory
+
+/**
+ *
+ * 在Spark中创建RDD的创建方式可以分为三种
+ *
+ * @author EASON
+ */
+object SparkCore_03_Action {
+
+  val logger = LoggerFactory.getLogger(this.getClass)
+
+  def main(args: Array[String]): Unit = {
+    Logger.getLogger("org.apache.hadoop").setLevel(Level.WARN)
+    Logger.getLogger("org.apache.spark").setLevel(Level.WARN)
+    Logger.getLogger("org.spark-project").setLevel(Level.WARN)
+
+    //创建conf对象
+    val conf = new SparkConf()
+      .setAppName("SparkCore_03_Action")
+      .setMaster("local")
+    val sc = new SparkContext(conf)
+    //  reduce 通过func函数聚集RDD中的所有元素，先聚合分区内数据，再聚合分区间数据。
+    reduceOpt(sc)
+    // collect 在驱动程序中，以数组的形式返回数据集的所有元素。
+    collectOpt(sc)
+  }
+
+  /**
+   * reduce 通过func函数聚集RDD中的所有元素，先聚合分区内数据，再聚合分区间数据。
+   *
+   * @param sc
+   * @return
+   */
+  def reduceOpt(sc: SparkContext): Unit = {
+    val rdd1 = sc.makeRDD(1 to 10,2)
+    logger.warn("reduceOpt -> {}",  rdd1.reduce(_+_))
+
+    val rdd2 = sc.makeRDD(Array(("a",1),("a",3),("c",3),("d",5)))
+    logger.warn("reduceOpt -> {}", rdd2.reduce((x,y)=>(x._1 + y._1, x._2 + y._2)))
+  }
+
+  /**
+   * collect 在驱动程序中，以数组的形式返回数据集的所有元素。
+   *
+   * @param sc
+   * @return
+   */
+  def collectOpt(sc: SparkContext): Unit = {
+    val rdd = sc.makeRDD(1 to 10,2)
+    logger.warn("collect -> {} count-> {}",  rdd.collect(), rdd.count)
+    logger.warn("first -> {} take 1-> {}",  rdd.first, rdd.take(3))
+    // 将该RDD所有元素相加得到结果
+    logger.warn("aggregate -> {}",  rdd.aggregate(0)(_+_,_+_))
+    // fold aggregate的简化操作，seqop和combop一样的时候,可以使用fold
+    logger.warn("fold -> {}",  rdd.fold(0)(_+_))
+    rdd.foreach(println(_))
+
+
+    val rdd2 = sc.parallelize(List((1,3),(1,2),(1,4),(2,3),(3,6),(3,8)),3)
+    logger.warn("countByKey -> {}",  rdd2.countByKey)
+    rdd2.foreach(println(_))
+
+  }
+
+
+}
