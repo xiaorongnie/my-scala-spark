7db3c1bd366469914be3a2eecc262fbc303c9981
xiaorongnie <xiaorongnie@transcodegroup.com>
2021-11-01 16:20:23 +0800

    增加SparkSession与DataFrame转换

--- src/main/scala/com/transcodegroup/sparksql/SparkSql_1_DataFrame.scala
+++ src/main/scala/com/transcodegroup/sparksql/SparkSql_1_DataFrame.scala
@@ -1,9 +1,9 @@
 package com.transcodegroup.sparksql
 
 import org.apache.log4j.{Level, Logger}
+import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
 import org.slf4j.LoggerFactory
-
 
 /**
  *
@@ -26,5 +26,29 @@ object SparkSql_1_DataFrame {
       .getOrCreate()
     var df = sparkSession.read.json("./data/fleet.json")
     df.show()
+
+    // 对DataFrame创建一个临时表, 临时视图只能在当前 Session 有效, 在新的 Session 中无效.
+    df.createOrReplaceTempView("fleet")
+    sparkSession.sql("select * from fleet").show
+
+    // 可以创建全局视图. 访问全局视图需要全路径:如global_temp.xxx
+    df.createGlobalTempView("people")
+    sparkSession.sql("select * from global_temp.people").show()
+    sparkSession.newSession.sql("select * from global_temp.people").show()
+
+    // DSL 语法风格(了解)
+    df.printSchema
+    df.select("msg").show
+
+    // RDD 和 DataFrame 的交互
+    var rdd = df.rdd
+    // 从 DataFrame到RDD
+    rdd.collect().foreach(println)
+
+    // RDD到DataFrame
+    var rdd2 = sparkSession.sparkContext.parallelize(List(("lisi", 10), ("zs", 20), ("zhiling", 40)))
+    // 隐式转换
+    import sparkSession.implicits._
+    rdd2.toDF().show
   }
 }
